f2d79f9e6543b815f1c493769d8f6470a89e7e6f
hsab <hsab@users.noreply.github.com>
2023-08-27 17:24:12 -0700

    Style: design-matched dropdown styles

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -7,50 +7,55 @@ const Dropdown = ({ options }: { options: string[] }) => {
   const [index, setIndex] = useState(0);
 
   return (
-    <div className="relative inline-block min-w-[170px] text-left">
+    <div className="relative inline-block min-w-[164px] text-left">
       <div className="group">
         <button
           type="button"
-          className="inline-flex w-full flex-row items-center  justify-between rounded-normal border border-transparent bg-gray-200 px-2 py-1 font-semibold text-gray-800 duration-300 ease-in-out hover:bg-gray-500 hover:text-white focus:outline-none"
+          className="bg-white-dark inline-flex flex-row items-center gap-[2px] rounded-[4px] p-2 pl-3 text-base font-medium text-black duration-300 hover:bg-gray-600 hover:text-white focus:outline-none"
           onClick={() => setIsOpen(!isOpen)}
         >
-          <div>{options[index]}</div>
-          <div
-            className={`ml-2  h-[6px] w-[6px]  border-b border-l border-gray-600 duration-300 ease-in-out group-hover:border-white ${
-              isOpen
-                ? 'translate-y-[1px] rotate-[135deg]'
-                : 'translate-y-[-1px] -rotate-45'
-            }`}
-          />
+          <div className="leading-5 text-base">{options[index]}</div>
+          <div className="flex h-4 w-4 flex-col items-center justify-center ">
+            <div
+              className={`h-[6px] w-[6px]  border-b border-l border-black duration-300 group-hover:border-white ${
+                isOpen
+                  ? 'translate-y-[1px] rotate-[135deg]'
+                  : 'translate-y-[-1px] -rotate-45'
+              }`}
+            />
+          </div>
         </button>
       </div>
 
-      {isOpen && (
-        <div className="absolute left-0 mt-2 w-full origin-top-right rounded-normal bg-gray-200 ">
-          <div
-            className="flex flex-col py-1"
-            role="menu"
-            aria-orientation="vertical"
-            aria-labelledby="options-menu"
-          >
-            {options.map((option, idx) => {
-              return (
-                <button
-                  key={option}
-                  className="mx-1 block whitespace-nowrap rounded-normal px-2 py-2 text-sm text-gray-700 duration-300 hover:bg-gray-100 hover:text-gray-900"
-                  role="menuitem"
-                  onClick={() => {
-                    setIndex(idx);
-                    setIsOpen(false);
-                  }}
-                >
-                  {option}
-                </button>
-              );
-            })}
-          </div>
+      <div
+        className={`bg-white-dark absolute left-0 mt-2 w-full origin-top-right rounded-normal p-1 duration-300  ${
+          isOpen ? 'scale-y-1 shadow-center' : 'scale-y-0 shadow-none'
+        }`}
+      >
+        <div
+          className="flex flex-col items-start"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="options-menu"
+        >
+          {options.map((option, idx) => {
+            return (
+              <button
+                key={option}
+                className="w-full whitespace-nowrap rounded-normal px-2 py-2 text-left text-sm text-gray-700 duration-200 hover:bg-gray-600  hover:text-white"
+                role="menuitem"
+                onClick={() => {
+                  setIndex(idx);
+                  setIsOpen(false);
+                }}
+              >
+                {option}
+              </button>
+            );
+          })}
         </div>
-      )}
+      </div>
+      {isOpen && <div />}
     </div>
   );
 };
