6ce281ca4ec109e32297440476c6d0b70e150b94
hsab <hsab@users.noreply.github.com>
2023-08-27 23:23:34 -0700

    FINAL

---
+++ app/[post]/page.tsx
@@ -0,0 +1,212 @@
+'use client';
+
+import {
+  Unsubscribe,
+  addDoc,
+  collection,
+  doc,
+  increment,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+  updateDoc,
+} from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import Post from '~/components/Post/Post';
+import { db } from '~/libs/firebase';
+import { CommentType, PostType } from '~/types';
+import { RiOpenaiFill } from 'react-icons/ri';
+import { ImSpinner8 } from 'react-icons/im';
+import { IoSend } from 'react-icons/io5';
+import MetaHeader from '~/components/Post/MetaHeader';
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: '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',
+  dangerouslyAllowBrowser: true,
+});
+
+const Page = ({ params }: { params: { post: string } }) => {
+  const [post, setPost] = useState<PostType | undefined>(undefined);
+  const [comments, setComments] = useState<CommentType[]>([]);
+
+  const [name, setName] = useState<string>('Anonymous User');
+  const [comment, setComment] = useState<string>('');
+
+  const [isCompleting, setIsCompleting] = useState<boolean>(false);
+
+  useEffect(() => {
+    let unsub1: Unsubscribe | undefined;
+    let unsub2: Unsubscribe | undefined;
+    if (params.post) {
+      unsub1 = onSnapshot(doc(db, 'posts', params.post), (currDoc) => {
+        console.log('Posts ', currDoc.data());
+        setPost({ ...currDoc.data(), id: params.post } as PostType);
+      });
+
+      const docRef = doc(db, 'posts', params.post);
+      const q = query(
+        collection(docRef, 'comments'),
+        orderBy('createdAt', 'desc'),
+      );
+
+      unsub1 = onSnapshot(q, (snapshot) => {
+        const currComments: CommentType[] = [];
+        snapshot.forEach((currDoc) => {
+          currComments.push({
+            id: currDoc.id,
+            ...currDoc.data(),
+          } as CommentType);
+        });
+        setComments(currComments);
+        console.log('Comments ', currComments);
+      });
+    }
+
+    return () => {
+      unsub1?.();
+      unsub2?.();
+    };
+  }, []);
+
+  const completeComment = async () => {
+    const prompt = `
+	The following is a post in JSON from a participatory budgeting platform. 
+
+	${JSON.stringify(post, null, 2)}
+	
+	Generate a unique and thoughtful comment as well as a unique and diverse name in the following JSON format within 240 characters. 	Ensure JSON is valid
+	
+	{
+	  name: string,
+	  comment: string
+	}
+	`;
+
+    setIsCompleting(true);
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: 'user',
+          content: prompt,
+        },
+      ],
+      model: 'gpt-3.5-turbo',
+    });
+    setIsCompleting(false);
+
+    try {
+      const obj = JSON.parse(completion.choices?.[0]?.message.content || '');
+      setComment(obj.comment);
+      setName(obj.name);
+    } catch (error) {
+      /* empty */
+    }
+  };
+
+  const buttonClick = async () => {
+    if (post && comment !== '' && comment) {
+      const docRef = doc(db, 'posts', post.id);
+      const data: Partial<CommentType> = {
+        name,
+        description: comment,
+      };
+
+      await addDoc(collection(docRef, 'comments'), {
+        ...JSON.parse(JSON.stringify(data)),
+        createdAt: serverTimestamp(),
+        likes: 0,
+      });
+
+      await updateDoc(doc(db, 'posts', params.post), {
+        comments: increment(1),
+      });
+
+      setComment('');
+      setName('');
+    } else if (post) {
+      completeComment();
+    }
+  };
+
+  if (!post) return <div />;
+
+  return (
+    <main className="flex flex-col justify-start w-full min-h-screen bg-white">
+      {post && <Post post={post} />}
+
+      <div>
+        {comments.map((currComment) => {
+          return (
+            <div key={currComment.id} className="p-4 border-t">
+              <MetaHeader post={currComment as unknown as PostType} />
+
+              <div className="mt-2 text-xs text-charcoal">
+                {currComment.description}
+              </div>
+
+              <div className="flex flex-row gap-4 mt-2 text-xs text-blue-dark">
+                <button
+                  onClick={async (e) => {
+                    e.preventDefault();
+
+                    if (params.post) {
+                      await updateDoc(
+                        doc(
+                          db,
+                          'posts',
+                          params.post,
+                          'comments',
+                          currComment.id,
+                        ),
+                        {
+                          likes: increment(1),
+                        },
+                      );
+                    }
+                  }}
+                >
+                  {currComment.likes} Likes
+                </button>
+                <div>Reply</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="fixed bottom-0 flex flex-row w-full gap-2 p-4 border-t">
+        <textarea
+          value={comment}
+          placeholder="Write a comment"
+          className="box-border w-full p-3 text-sm leading-4 max-h-[40px] text-black border placeholder-gray-dark rounded-normal border-gray-dark"
+          rows={1}
+          style={{
+            resize: 'none',
+          }}
+          onChange={(e) => setComment(e.target.value)}
+        />
+
+        <button
+          className="flex flex-col items-center justify-center h-10 aspect-square bg-blue-light rounded-normal"
+          onClick={async (e) => {
+            e.preventDefault();
+            buttonClick();
+          }}
+        >
+          {/* eslint-disable-next-line no-nested-ternary */}
+          {isCompleting ? (
+            <ImSpinner8 color="#0396A6" size="22px" className="animate-spin" />
+          ) : comment === '' ? (
+            <RiOpenaiFill color="#0396A6" size="22px" />
+          ) : (
+            <IoSend color="#0396A6" size="20px" />
+          )}
+        </button>
+      </div>
+    </main>
+  );
+};
+
+export default Page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,84 @@
 'use client';
 
+import { db } from '~/libs/firebase';
 import Header from '~/components/Header';
-import Post from '~/components/Post';
+
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+
+import { useEffect, useState } from 'react';
+import BottomSheet from '~/components/BottomSheet';
+import { PostType } from '~/types';
+import RequestForm from '~/components/RequestForm/RequestForm';
+import PostCard from '~/components/Post/PostCard';
+import Link from 'next/link';
 
 const Page = () => {
+  const [open, setOpen] = useState(false);
+  const [posts, setPosts] = useState<PostType[]>([]);
+
+  useEffect(() => {
+    const q = query(collection(db, 'posts'), orderBy('createdAt', 'desc'));
+
+    const unsub = onSnapshot(q, (snapshot) => {
+      const currPosts: PostType[] = [];
+      snapshot.forEach((doc) => {
+        currPosts.push({ id: doc.id, ...doc.data() } as PostType);
+      });
+      setPosts(currPosts);
+      console.log('Posts ', currPosts);
+    });
+
+    return () => {
+      unsub();
+    };
+  }, []);
+
   return (
-    <main className="flex flex-col justify-start w-full min-h-screen bg-white">
-      <Header />
+    <>
+      <main className="flex flex-col justify-start w-full min-h-screen bg-white">
+        <Header />
 
-      <div className="grid grid-cols-4 text-sm leading-4 text-charcoal">
-        <div className="flex flex-row justify-center items-center font-medium py-4  border-b-2 border-[#333333] ">
-          Recent
+        <div className="grid grid-cols-4 text-sm leading-4 text-charcoal">
+          <div className="flex flex-row justify-center items-center font-medium py-4  border-b-2 border-[#333333] ">
+            Recent
+          </div>
+          <div className="flex flex-row items-center justify-center py-4">
+            Trending
+          </div>
+          <div className="flex flex-row items-center justify-center py-4">
+            Editable
+          </div>
+          <div className="flex flex-row items-center justify-center py-4">
+            Restricted
+          </div>
         </div>
-        <div className="flex flex-row items-center justify-center py-4">
-          Trending
-        </div>
-        <div className="flex flex-row items-center justify-center py-4">
-          Editable
-        </div>
-        <div className="flex flex-row items-center justify-center py-4">
-          Restricted
-        </div>
-      </div>
 
-      <Post />
-      <Post />
-      <button className="absolute bottom-0 right-0 flex flex-col items-center justify-center h-10 px-3 mb-5 mr-6 text-sm rounded-lg bg-blue-light text-blue-dark">
-        Make a request
-      </button>
-    </main>
+        <div>
+          {posts.map((post) => {
+            return (
+              <Link href={`/${post.id}`} key={post.id}>
+                <PostCard key={post.id} post={post} />
+              </Link>
+            );
+          })}
+        </div>
+
+        <button
+          className="fixed bottom-0 right-0 flex flex-col items-center justify-center h-10 px-3 mb-5 mr-6 text-sm rounded-lg bg-blue-light text-blue-dark"
+          onClick={() => setOpen(true)}
+        >
+          Make a request
+        </button>
+      </main>
+
+      <BottomSheet open={open} setOpen={setOpen}>
+        <RequestForm
+          onSubmitCallback={() => {
+            setOpen(false);
+          }}
+        />
+      </BottomSheet>
+    </>
   );
 };
 
