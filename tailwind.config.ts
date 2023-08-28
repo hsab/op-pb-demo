62d166af1cd0d83c9e22b58f7c5b8cd4f0080d00
hsab <hsab@users.noreply.github.com>
2023-08-27 17:23:25 -0700

    Style: Custom Tailwind vars

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,8 +8,23 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        xxs: '0.625rem',
+      },
+      colors: {
+        black: '#142326',
+        charcoal: '#394548',
+        'white-dark': '#edeeee',
+        'gray-dark': '#606a6c',
+        'blue-dark': '#0396A6',
+        'blue-light': '#E0F2F4',
+        teal: '#0396a6',
+      },
+      boxShadow: {
+        center: '0 10px 20px 5px rgba(0, 0, 0, 0.3)',
+      },
       borderRadius: {
-        normal: '0.2rem',
+        normal: '0.25rem',
       },
     },
   },
